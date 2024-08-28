af968a68281e606215363daba68957ee4d1287ec
AJAY <ajayvnair61@gmail.com>
2024-08-28 12:45:35 +0530

    pic fixed??

--- Grievance_site/src/Pages/Home/Home.jsx
+++ Grievance_site/src/Pages/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   <>
     <Navbar />
     <div className="home-container">
-        <img className='img' src="\assets\pexels-julia-larson-6456305.jpg" alt="Grievance Portal" />
+        <img className='img' src=".\src\assets\pexels-julia-larson-6456305.jpg" alt="Grievance Portal" />
         <div className="title-overlay">
             <h1>Welcome to Our Grievance Portal</h1>
             <p>Your Voice Matters</p>
